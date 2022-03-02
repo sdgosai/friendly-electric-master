8931192b4f4209f3835d938bbe5a017723df0452
sachingosai <sachingosai.flexion@gmail.com>
2022-03-02 19:19:02 +0530

    add razorpay config

--- src/controller/userController.js
+++ src/controller/userController.js
@@ -8,7 +8,7 @@ require('dotenv').config()
 require('../middleware/fbAuth')(passport)
 const { TokenSender } = require('../utils/tokenSender')
 
-var instance = new rzp({  key_id: process.env.key_id,  key_secret: process.env.key_secret,});
+var instance = new rzp({ key_id: process.env.key_id, key_secret: process.env.key_secret, });
 
 exports.addUser = async (req, res) => {
     const { name, email, number } = req.body;
@@ -247,22 +247,22 @@ exports.razorpayWallet = async (req, res) => {
     const rzpOrder = await rzp.orders.create({
         amount: amount * 100, // rzp format with paise
         currency: 'INR',
-        receipt: "receipt#1", //Receipt no that corresponds to this Order,
-        // payment_capture: true,
+        receipt: "receipt01", //Receipt no that corresponds to this Order,
+        payment_capture: true,
         notes: {
-         orderType: "Pre"
-        } 
-       //Key-value pair used to store additional information
-       });
-       // To create recurring subscription
-       const subscriptionObject = {
-        // plan_id: PLAN_ID,
+            orderType: "Pre"
+        }
+        //Key-value pair used to store additional information
+    });
+    // To create recurring subscription
+    const subscriptionObject = {
+        plan_id: plan_id,
         total_count: 60,
         quantity: 1,
         customer_notify: 1,
         notes,
-       }
-       const subscription = await rzp.subscriptions.create(subscriptionObject);
+    }
+    const subscription = await rzp.subscriptions.create(subscriptionObject);
 
     //    if(err) {
     //        res.send({
